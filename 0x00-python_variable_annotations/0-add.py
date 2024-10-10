4d04829374df3937a75135a3fb48dff99341151f
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-10-10 17:11:02 +0100

    created README and solution for task #0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+"""Module for adding two numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two numbers.
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float: The sum of a and b.
+    """
+
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+# Project: 0x00-python_variable_annotations
